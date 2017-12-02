d82acb6c7b260c1718f0ca5f660184031075fba2
박민현 <algusl92@naver.com>
2017-12-03 03:50:18 +0900

    기본적인 입력 디자인 완성

---
+++ URLshorting/app/src/main/java/com/example/parkminhyun/urlshorten/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.parkminhyun.urlshorten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText inputURL_EditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        propertyInit();
+    }
+
+    private void propertyInit() {
+        inputURL_EditText = (EditText)findViewById(R.id.editURL);
+    }
+
+    public void deleteBtnClicked(View view) {
+        inputURL_EditText.setText("");
+    }
+
+    public void findFoodStoreBtnClicked(View view) {
+    }
+}
