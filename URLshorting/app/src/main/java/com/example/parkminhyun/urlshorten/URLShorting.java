0310be2d85963ca5882038cd61c5b0e920d882b2
박민현 <algusl92@naver.com>
2017-12-03 12:11:06 +0900

    URL Shorting 기능 완성

--- URLshorting/app/src/main/java/com/example/parkminhyun/urlshorten/URLShorting.java
+++ URLshorting/app/src/main/java/com/example/parkminhyun/urlshorten/URLShorting.java
@@ -44,4 +44,79 @@ public class URLShorting {
         }
     }
 
+    // URL Sorting Method
+    public String shortenURL(String url) {
+        String shortURL = "";
+
+        // url 유효 검사
+        if (validateURL(url)) {
+
+            // url substring 기능 ex) http://만 남도록
+            String transformedURL = subStringURL(url);
+
+            // 이미 key에 있는 url인지 확인
+            if (valueMap.containsKey(url)) {
+                shortURL = shortenDomain + "/" + valueMap.get(transformedURL);
+            }
+            // 등록된 url이 없으면 새로 생성
+            else {
+                shortURL = shortenDomain + "/" + getKey(transformedURL);
+            }
+        }
+
+        return shortURL;
+    }
+
+    // URL 예외처리 검사 기능 Method - 아직 미구현
+    public boolean validateURL(String url) {
+        return true;
+    }
+
+
+    String subStringURL(String url) {
+
+        // "http://" 문자 담기
+        if (url.substring(0, 7).equals("http://"))
+            url = url.substring(7);
+
+        // 마지막 '/' 포함 된 경우 삭제
+        if (url.charAt(url.length() - 1) == '/')
+            url = url.substring(0, url.length() - 1);
+
+        return url;
+    }
+
+    // url에 대한 key 부여 Method
+    private String getKey(String url) {
+        String key;
+
+        // key 생성
+        key = generateKey();
+        // url shorten을 위한 hashmap에 key,url 담기
+        keyMap.put(key, url);
+        // redirect를 위한 hashmap에 url,key 담기
+        valueMap.put(url, key);
+        return key;
+    }
+
+    // Key 생성 Method
+    private String generateKey() {
+        String key = "";
+        boolean flag = true;
+
+        // 중복되는 shorten url이 없을 때 까지 반복
+        while (flag) {
+            key = "";
+            // key값 생성
+            for (int i = 0; i < keyLength; i++) {
+                key += shortenKey[myRand.nextInt(62)];
+            }
+            // 만든 key가 없는 key일 경우 반복문 종료
+            if (!keyMap.containsKey(key)) {
+                flag = false;
+            }
+        }
+        return key;
+    }
+
 }
